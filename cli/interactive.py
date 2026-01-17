8d3c8e486eac2cf6f95687dfee349c00a712250e
Nawan <z5527177@ad.edu.unsw.au>
2026-01-17 14:56:41 +0800

    setup file structure

---
+++ cli/interactive.py
@@ -0,0 +1,16 @@
+from InquirerPy import prompt
+
+questions = [
+    {"type": "input", "message": "What's your name:", "name": "name"},
+    {
+        "type": "list",
+        "message": "What's your favourite programming language:",
+        "choices": ["Go", "Python", "Rust", "JavaScript"],
+    },
+    {"type": "confirm", "message": "Confirm?"},
+]
+result = prompt(questions)
+print(result)
+name = result["name"]
+fav_lang = result[1]
+confirm = result[2]
