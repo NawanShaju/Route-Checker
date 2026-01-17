6d591d2d1e665c23c7af8b494c68bc0fcf827d0e
nishanshaju22 <nishan.shaju@icloud.com>
2026-01-17 15:35:22 +0800

    interactives file updated

--- cli/interactive.py
+++ cli/interactive.py
@@ -1,16 +1,28 @@
 from InquirerPy import prompt
+from InquirerPy.base.control import Choice
 
-questions = [
-    {"type": "input", "message": "What's your name:", "name": "name"},
-    {
-        "type": "list",
-        "message": "What's your favourite programming language:",
-        "choices": ["Go", "Python", "Rust", "JavaScript"],
-    },
-    {"type": "confirm", "message": "Confirm?"},
-]
-result = prompt(questions)
-print(result)
-name = result["name"]
-fav_lang = result[1]
-confirm = result[2]
+
+def userQuaries():
+    questions = [
+        {
+            "type": "list",
+            "message": "what Backend framework are you using:",
+            "choices": [
+                Choice("express", name="Express (JavaScript)"),
+                Choice("fastapi", name="FastAPI (Python)"),
+                Choice("flask", name="Flask (Python)"),
+            ],
+            "name": "backend",
+        },
+        {
+            "type": "list",
+            "message": "what frontend framework are you using:",
+            "choices": [
+                Choice("react", name="React (JavaScript)"),
+                Choice("nextjs", name="Next.js (JavaScript)"),
+            ],
+            "name": "frontend",
+        },
+    ]
+
+    return prompt(questions)
