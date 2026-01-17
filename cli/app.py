8cd08244bd595f04dcd48499ca0a8e345f1971b6
nishanshaju22 <nishan.shaju@icloud.com>
2026-01-17 15:49:43 +0800

    messages added for user

--- cli/app.py
+++ cli/app.py
@@ -1,8 +1,22 @@
 from cli.interactive import userQuaries
+import sys
 
 def app():
-    result = userQuaries()
+    
+    print("Welcome to Route Scanner.")
+    print("Please answer the questions below to continue")
+    
+    try:
+        result = userQuaries()
+    except:
+        print("The quaring has failed please run the code again.")
+        sys.exit(1)
+        
+    
     backend = result['backend']
     frontend = result['frontend']
     
+    
+    print(backend)
+    print(frontend)
     
